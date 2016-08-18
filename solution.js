8274d07dbd7e5771fcc58fec32d03ec6c3522367
koyexes <koyagabriel@gmail.com>
2016-08-18 21:59:48 +0100

    Solution

---
+++ solution.js
@@ -0,0 +1,12 @@
+var anArray = [1,2,2,1,3,3,3,4,1,2,2,4,4,4,5];
+
+var getNumberWithFrequencyOfOne = function(arrayParam) {
+	for (var i = 0; i < arrayParam.length; i++) {
+		
+		if (arrayParam.indexOf(arrayParam[i]) == arrayParam.lastIndexOf(arrayParam[i])) {
+			return arrayParam[i];
+		}
+	}
+}
+
+console.log(getNumberWithFrequencyOfOne(anArray));
